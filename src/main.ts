4577be9d088dcdcd5595f1f8fa0424afad33d3b1
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-09-02 21:37:08 +0200

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import {renderWithQiankun, qiankunWindow, type QiankunProps} from 'vite-plugin-qiankun/dist/helper'
+import './app.css'
+import App from './App.svelte'
+
+let app
+
+const retrieveContainer = (props: QiankunProps) => (props.container || document).querySelector('#app')
+
+const render = (props = {}) => {
+  app = new App({
+    target: retrieveContainer(props),
+    props
+  })
+}
+
+renderWithQiankun({
+  mount(props) {
+    render(props)
+  },
+  bootstrap() { },
+  unmount() {
+    app.$destroy()
+  },
+  update() {}
+})
+
+if (!qiankunWindow.__POWERED_BY_QIANKUN__) {
+  render({})
+}
+
+export default app
