34fd560724807acfe925e5b7b2185560dfa6b2ff
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-11-15 09:04:16 +0100

    css injection

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import './app.css'
 import App from './App.svelte'
 
-import OrchyBaseMfe from '@orchy-mfe/spa-adapter'
+import OrchyMicroFrontend from '@orchy-mfe/spa-adapter'
 import type {MicroFrontendProperties} from '@orchy-mfe/models'
-export class SvelteMfeTypeScript extends OrchyBaseMfe {
+export class SvelteMfeTypeScript extends OrchyMicroFrontend {
   private app: App
   async mount(microFrontendProperties: MicroFrontendProperties) {
     this.app = new App({
